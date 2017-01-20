2998c1991d2abab950ba0930a85a03fbfc1af8b9
ocastillo9 <oscarcastillov@gmail.com>
2017-01-20 18:22:05 -0300

    Rest changes

--- src/main/java/com/boot/controller/HomeController.java
+++ src/main/java/com/boot/controller/HomeController.java
@@ -2,15 +2,17 @@ package com.boot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by oscar.castillo on 13/01/2017.
  */
 @RestController
+@RequestMapping("/")
 public class HomeController {
 
-    @RequestMapping("/")
+    @RequestMapping(path = "/test", method = RequestMethod.POST)
     public String home(){
         return "Das Boot, reporting for duty!";
     }
